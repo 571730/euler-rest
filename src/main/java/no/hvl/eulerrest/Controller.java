d36be02c1c37e5d85fe50c53ff0f3d7893dcbc7b
571730 <571730@stud.hvl.no>
2019-03-25 21:46:23 +0100

    byttet ut med Autowired, ingen fix

--- src/main/java/no/hvl/eulerrest/Controller.java
+++ src/main/java/no/hvl/eulerrest/Controller.java
@@ -1,6 +1,7 @@
 package no.hvl.eulerrest;
 
 import no.hvl.eulerrest.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/euler")
 public class Controller {
-    @Resource
+    @Autowired
     QuestionService qService;
 
     @GetMapping(value = "/questions")
--- src/main/java/no/hvl/eulerrest/service/QuestionServiceImpl.java
+++ src/main/java/no/hvl/eulerrest/service/QuestionServiceImpl.java
@@ -2,6 +2,7 @@ package no.hvl.eulerrest.service;
 
 import no.hvl.eulerrest.Question;
 import no.hvl.eulerrest.interfaces.QuestionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Component
 public class QuestionServiceImpl implements QuestionService{
-    @Resource
+    @Autowired
     QuestionDAO qDAO;
 
     @Override
