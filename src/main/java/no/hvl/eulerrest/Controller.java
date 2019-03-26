2aea9ad378f070edc5ea47de792c10a5644678ee
571730 <571730@stud.hvl.no>
2019-03-26 11:05:09 +0100

    it's alive

--- src/main/java/no/hvl/eulerrest/Controller.java
+++ src/main/java/no/hvl/eulerrest/Controller.java
@@ -10,7 +10,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/euler")
+@RequestMapping("/")
 public class Controller {
     @Autowired
     QuestionService qService;
--- src/main/java/no/hvl/eulerrest/ServletInitializer.java
+++ src/main/java/no/hvl/eulerrest/ServletInitializer.java
@@ -1,7 +1,8 @@
 package no.hvl.eulerrest;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
